68746f843fe5dfb8c8b8949a9effeca3268505a1
noyvirto <106173842+krkenCoding@users.noreply.github.com>
2022-12-05 10:49:59 +0000

    removed the redundant data

--- day5/day5.py
+++ day5/day5.py
@@ -1,16 +1,6 @@
 # Unfortunately crates position/stack hard coded by hand
 # Columns were then deleted from the input file
 
-column1 = ["R", "P", "C", "D", "B", "G"]
-column2 = ["H", "V", "G"]
-column3 = ["N", "S", "Q", "D", "J", "P", "M"]
-column4 = ["P", "S", "L", "G", "D", "C", "N", "M"]
-column5 = ["J", "B", "N", "C", "P", "F", "L", "S"]
-column6 = ["Q", "B", "D", "Z", "V", "G", "T", "S"]
-column7 = ["B", "Z", "M", "H", "F", "T", "Q"]
-column8 = ["C", "M", "D", "B", "F"]
-column9 = ["F", "C", "Q", "G"]
-
 columnStacks = {"column1": ["R", "P", "C", "D", "B", "G"], "column2": ["H", "V", "G"],
         "column3": ["N", "S", "Q", "D", "J", "P", "M"], "column4": ["P", "S", "L", "G", "D", "C", "N", "M"],
         "column5": ["J", "B", "N", "C", "P", "F", "L", "S"], "column6": ["Q", "B", "D", "Z", "V", "G", "T", "S"],
